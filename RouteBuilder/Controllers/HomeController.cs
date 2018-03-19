37753acdad940e6288381fce9955536b8cab1bad
Shawn <USSHAVIN@corp.ottobock.int>
2018-03-19 11:23:48 -0600

    Adding files for first time

---
+++ RouteBuilder/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebTest.Models;
+
+namespace WebTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewRoute()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult MyRoutes()
+        {
+            ViewData["Message"] = "My Routes page.";
+
+            return View();
+        }
+
+        public IActionResult MyWalls()
+        {
+            ViewData["Message"] = "My Walls page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RouteBuilder/Controllers/SignedInController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class SignedInController : Controller
+    {
+        // 
+        // GET: /SignedIn/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /SignedIn/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+    
+}
